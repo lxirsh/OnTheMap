b6ade2fb54ca52283214f7371c3951976e4e8f33
Lance Hirsch <lxirsh@gmail.com>
2016-03-24 17:47:15 +0200

    Add a view controller for the map view and make it its delegate.

---
+++ OnTheMap/MapViewViewController.swift
@@ -0,0 +1,38 @@
+//
+//  MapViewViewController.swift
+//  OnTheMap
+//
+//  Created by Lance Hirsch on 3/24/16.
+//  Copyright © 2016 Lance Hirsch. All rights reserved.
+//
+
+import UIKit
+import MapKit
+
+class MapViewViewController: UIViewController, MKMapViewDelegate {
+
+    @IBOutlet weak var mapView: MKMapView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
