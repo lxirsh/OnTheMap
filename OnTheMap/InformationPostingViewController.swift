b0fc3705e9eac6ab5aeb074c3402da075c79d900
Lance Hirsch <lxirsh@gmail.com>
2016-04-03 11:11:20 +0300

    Create a configureUIForState method for the information view controller to show and hide UI elements.

--- OnTheMap/InformationPostingViewController.swift
+++ OnTheMap/InformationPostingViewController.swift
@@ -12,13 +12,48 @@ import MapKit
 class InformationPostingViewController: UIViewController, MKMapViewDelegate{
 
     @IBOutlet weak var mapView: MKMapView!
+    @IBOutlet weak var middleView: UIView!
+    @IBOutlet weak var bottomView: UIView!
+    @IBOutlet weak var topTextField: UITextField!
+    @IBOutlet weak var middleTextField: UITextField!
+    @IBOutlet weak var button: UIButton!
     
+    enum UIState: String {
+        case Initial, Searching, LocationView
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
-        mapView.alpha = 0
+//        mapView.alpha = 0
+        configureUIForState(.Initial)
+    }
+    
+    @IBAction func butonTouched(sender: AnyObject) {
+        configureUIForState(.LocationView)
+    }
+    
+    func configureUIForState(state: UIState) {
+        
+        switch state {
+        case .Initial:
+            print("Initial")
+            middleView.alpha = 1
+            bottomView.alpha = 1
+            mapView.alpha = 0
+            topTextField.enabled = false
+        case .Searching:
+            print("Searching")
+        case .LocationView:
+            middleView.alpha = 0
+            bottomView.alpha = 0.5
+            mapView.alpha = 1
+            topTextField.text = "Enter a Link to Share Here"
+            topTextField.enabled = true
+            button.setTitle("Submit", forState: .Normal)
+            print("Location view")
+        }
     }
 
     override func didReceiveMemoryWarning() {
