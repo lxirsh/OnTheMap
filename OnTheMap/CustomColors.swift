03925b898d450598564a7b5ce9c02b526614d943
Lance Hirsch <lxirsh@gmail.com>
2016-06-05 15:02:36 +0300

    Make the grey text color for the information posting view lighter.

--- OnTheMap/CustomColors.swift
+++ OnTheMap/CustomColors.swift
@@ -12,18 +12,15 @@ class CustomColor {
     
     // MARK: Login View Controller Colors
     struct Login {
-//        static let TextFieldColor = UIColor(red: 1.0, green: 1.0, blue: 0.80, alpha: 0.5)
         static let TextFieldColor = UIColor(red: 255/255.0, green: 255/255.0, blue: 225/255.0, alpha: 0.5)
-
         static let LoginButtonColor = UIColor(red: 255/255.0, green: 80/255.0, blue: 0/255.0, alpha: 0.5)
-
     }
     
     // MARK: Information Posting View Controller Colors
     struct InformationPostingState {
         static let BackgroundBlueColor = UIColor(red: 79/255.0, green: 148/255.0, blue: 205/255.0, alpha: 1)
         static let BackgroundGrayColor = UIColor(red: 180/255.0, green: 180/255, blue: 180/255, alpha: 0.5)
-        static let TextGrayColor = UIColor(red: 210/255.0, green: 210/255.0, blue: 210/255.0, alpha: 1.0)
+        static let TextGrayColor = UIColor(red: 225/255.0, green: 225/255.0, blue: 225/255.0, alpha: 1.0)
         static let TextBlueColor = UIColor(red: 54/255.0, green: 100/255.0, blue: 139/255.0, alpha: 1)
         static let ButtonColor = UIColor(red: 240/255.0, green: 240/255, blue: 240/255, alpha: 1)
         static let CancelButtonColor = UIColor(red: 16/255.0, green: 78/255.0, blue: 139/255.0, alpha: 1)
